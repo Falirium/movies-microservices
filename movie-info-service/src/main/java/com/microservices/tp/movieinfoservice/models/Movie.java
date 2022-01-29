da5fa16b9fdc096cb4ccdf7ba9ce2cbb37e81533
falirium <mafarfaoua@insea.ac.ma>
2022-01-29 18:39:51 +0100

    fetch movies info from themoviedb via api_key

--- movie-info-service/src/main/java/com/microservices/tp/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/com/microservices/tp/movieinfoservice/models/Movie.java
@@ -3,16 +3,23 @@ package com.microservices.tp.movieinfoservice.models;
 public class Movie {
     private String movieId;
     private String name;
+    private String title;
+    private String overview;
 
     // testing things
 
-    public Movie(String movieId, String name) {
+    public Movie() {
+    }
+
+    public Movie(String movieId, String name, String title, String overview) {
         this.movieId = movieId;
         this.name = name;
+        this.title = title;
+        this.overview = overview;
     }
 
     public String getMovieId() {
-        return movieId;
+        return this.movieId;
     }
 
     public void setMovieId(String movieId) {
@@ -20,10 +27,28 @@ public class Movie {
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return this.overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+
 }
